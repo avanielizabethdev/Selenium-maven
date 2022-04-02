c391b434087051bc4a803303e1f1467e966932c7
Avani <avanielizabethdev@gmail.com>
2022-04-02 12:59:19 +0530

    first commit

---
+++ src/test/java/testcases/DataProviderExample.java
@@ -0,0 +1,23 @@
+package testcases;
+
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class DataProviderExample {
+	
+	@Test(dataProvider="dp-name")
+	public void PrintData(String data) {
+		
+		System.out.println("This data is:"+ data);
+	}
+	
+	@DataProvider(name="dp-name")
+	public Object[][] dpMethod(){
+		
+		return new Object[][] {
+			{"First-data"},
+			{"Second-data"}
+		};
+	}
+
+}
--- src/test/java/testcases/LoginTest.java
+++ src/test/java/testcases/LoginTest.java
@@ -10,10 +10,13 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import pages.LoginPage;
+
+@Listeners(TestNgListenerClass.class)
 
 public class LoginTest extends BaseClass{
 	
